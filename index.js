272a3c5fa2068f676df7cb09f353efe25e7be5fc
Fengda HUANG <h@phodal.com>
2016-04-05 21:59:51 +0800

    update index

--- index.js
+++ index.js
@@ -14,6 +14,14 @@ fs.readdir(process.cwd() + '/test_data', function (err, files) {
     files.forEach(function (filename) {
         fs.readFile('test_data/' + filename, function (err, origin_data) {
             var data = JSON.parse(origin_data);
+            var convertLatLang = function(data) {
+                data.location = {};
+                data.location["lat"] = data.latLang[0];
+                data.location["long"] = data.latLang[1];
+                return data
+            };
+            data = convertLatLang(data);
+
             client.index({
                 index: 'vmap',
                 type: 'userinfo',
