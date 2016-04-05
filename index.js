a48b2dd6b6a1e9d9a241f2306d4ac6abe61db754
Fengda HUANG <h@phodal.com>
2016-04-05 17:22:52 +0800

    elasticsearch hello world

---
+++ index.js
@@ -0,0 +1,16 @@
+var elasticsearch = require('elasticsearch');
+var client = new elasticsearch.Client({
+    host: 'localhost:9200',
+    log: 'trace'
+});
+
+client.ping({
+    requestTimeout: 30000,
+    hello: "elasticsearch"
+}, function (error) {
+    if (error) {
+        console.error('elasticsearch cluster is down!');
+    } else {
+        console.log('All is well');
+    }
+});
